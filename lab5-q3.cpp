e3aae172e808a5facc69b52005f44c6ff94c1400
anandacharan <42172651+anandacharan@users.noreply.github.com>
2018-09-03 17:20:55 +0000

    Add files via upload

---
+++ lab5-q3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a;
+  cout << "Enter any non-zero number :";
+  cin >>a;
+  (a>0)?cout <<"Number is positive" : cout << "Number is negative";
+  
+  return 0;
+}
